d9c6b16070925eb21b72898d357b123e62dd7172
daria021 <dariaedigareva@gmail.com>
2025-09-30 20:28:54 +0300

    hide public secrets

--- admin/backend/settings/service_account.py
+++ admin/backend/settings/service_account.py
@@ -7,7 +7,7 @@ from shared.settings import AbstractSettings
 class ServiceAccountSettings(AbstractSettings):
     api_id: int
     api_hash: str
-    session_string: SecretStr
+    session_string: SecretStr = Field(..., alias="SA_SESSION_STRING")
     proxy: Optional[str] = None
     service_bot_token: SecretStr = Field(..., alias="SERVICE_BOT_TOKEN")
     use_bot_for_service: bool = False
